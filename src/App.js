0583fedce3e7ad581e0134b6489869a78ac18940
caetanojoao11 <119068739+caetanojoao11@users.noreply.github.com>
2023-12-28 18:22:20 +0000

    mudanças na branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TESTE <code>src/App.js</code> and save ffdfdfto reload.
+          mudança na branch TESTE <code>src/App.js</code> and save ffdfdfto reload.
         </p>
         <a
           className="App-link"
