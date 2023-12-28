faae24b83af16cc020615798a1c7710ba3948ec6
caetanojoao11 <119068739+caetanojoao11@users.noreply.github.com>
2023-12-28 18:18:00 +0000

    A teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TESTE <code>src/App.js</code> and save ffdfdfto reload.
         </p>
         <a
           className="App-link"
